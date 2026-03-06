8685845831dfb2b45a7f4270a8d66ffc30d22cb5
Ashot <ashot.muradyan.ait@gmail.com>
2026-03-06 18:59:38 +0400

    creat div for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from "react";
 
 function Header() {
   return (
-        <div></div>
+        <div className= "flex gap-2">
+            <img src="https://www.stonemarket.am/icons/logo-primary.svg" alt="" />
+            <div></div>
+        </div>
   );
 }
 
