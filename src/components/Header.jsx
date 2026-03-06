83f7d57d9205b4536e847b581759745e02119c46
Ashot <ashot.muradyan.ait@gmail.com>
2026-03-06 21:42:01 +0400

    creat p for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-
 function Header() {
   return (
         <div className= "flex gap-2">
             <img src="https://www.stonemarket.am/icons/logo-primary.svg" alt="" />
-            <div></div>
+            <div>
+                <p className="justify-between">Գլխավոր</p>
+                <p>Խանութ</p>
+                <p>Դիզայներներ</p>
+                <p>Մեր մասին</p>
+                <p>Կապ</p>
+            </div>
         </div>
   );
-}
+}   
 
 export default Header;
