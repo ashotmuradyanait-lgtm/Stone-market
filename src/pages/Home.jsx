3ee12f74f5a5ac7d2b1d6e44aa5d587fb7875942
Ashot <ashot.muradyan.ait@gmail.com>
2026-03-06 23:28:03 +0400

    creat div for imgs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,12 @@ function Home() {
         <img src="https://www.stonemarket.am/_next/image?url=%2Fimages%2Frocket-line.jpg&w=1920&q=75" alt="" />
     </div>
     <div>
-        <h1></h1>
+        <h1 className="text-center font-bold font-serif text-3xl">Մեր Գործընկերները</h1>
+        <p className="text-center p-6">Մեր գործընկերները ներկայացնում են բարձրորակ քարեր և կոպեր, որոնք առաջարկում են նորարարական լուծումներ: <br /> 
+        Յուրաքանչյուր գործընկեր կարևոր է մեր առաքելության համար՝ ստեղծել գեղեցիկ միջավայրեր, որտեղ քարերը պատմում են <br />
+         իրենց պատմությունները:</p>
     </div>
+    <div></div>
     </>
   );
 }
