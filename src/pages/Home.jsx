a61873dfac02825c92a7e357722740a4c1d5b625
Ashot <ashot.muradyan.ait@gmail.com>
2026-03-06 23:20:22 +0400

    add h1 for Home.jsx card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,14 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-        <img src="" alt="" />
+    <>
+    <div className="p-20">
+        <img src="https://www.stonemarket.am/_next/image?url=%2Fimages%2Frocket-line.jpg&w=1920&q=75" alt="" />
     </div>
+    <div>
+        <h1></h1>
+    </div>
+    </>
   );
 }
 
