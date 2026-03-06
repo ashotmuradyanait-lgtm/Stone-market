d6c41344c33de7869f11710eaf4ed576cc286191
Ashot <ashot.muradyan.ait@gmail.com>
2026-03-06 22:15:28 +0400

    add img for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-        <div className= "flex gap-80 p-4">
+        <div className= "flex gap-60 p-6">
             <img src="https://www.stonemarket.am/icons/logo-primary.svg" alt="" />
             <div className="justify-between flex gap-8">
                 <p className="font-GHEAGrpalatReg font-bold">Գլխավոր</p>
@@ -10,6 +10,13 @@ function Header() {
                 <p className="font-GHEAGrpalatReg font-bold">Դիզայներներ</p>
                 <p className="font-GHEAGrpalatReg font-bold">Մեր մասին</p>
                 <p className="font-GHEAGrpalatReg font-bold">Կապ</p>
+            </div>
+            <div className="flex gap-6">
+                <i class="fa fa-search" aria-hidden="true"></i>
+                <i class="fa fa-sign-out" aria-hidden="true"></i>
+               <i class="fa fa-heart" aria-hidden="true"></i>
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                <img src="" alt="" />
             </div>
         </div>
   );
