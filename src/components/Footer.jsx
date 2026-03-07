858e750bf3c4cc31b5a7dc2496a48e108ae263a7
Ashot <ashot.muradyan.ait@gmail.com>
2026-03-07 11:52:42 +0400

    add img for Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import React, { useState } from "react";
 
 function Footer(){
-      const [checked, setChecked] = useState(false);
     return(
         <>
         <div className="bg-gray-900 h-[800px] w-[1520px]">
-            <div className="з-4 border border-white h-[450px] w-[500px] rounded-lg">
+            <div className="flex gap-6">
+            <div className="з-4 border border-white h-[400px] w-[500px] rounded-lg">
                 <p className="text-white font-bold text-2xl p-4">Հետադարձ կապ</p>
                 <p className="text-white text-sm">Լրացրեք տվյալները և մենք կկապնվենք Ձեզ հետ հնարավորինս <br />
                  շուտ։</p>
@@ -15,13 +14,15 @@ function Footer(){
                  <input className="text-white w-[400px] h-[35px] rounded-lg" type="number" placeholder="+374" />
                  <input className="text-white w-[400px] h-[35px] rounded-lg" type="text" placeholder="Կազմակերպություն" />
                  <br />
-                 <label className="flex items-center gap-2 cursor-pointer">
-      <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-2 focus:ring-blue-400"/>
-      <span className="text-gray-700 select-none">
-        Համաձայն եմ կայքի պայմաններին
-      </span>
-    </label>
+                 <span className="text-white">Համաձայն եմ կայքի պայմաններին</span>
+             <div className="justify-center flex p-20">
+                <p className="text-white bg-green-400 h-[40px] text-center w-[100px] rounded-lg mx-auto hover:bg-green-600 ">Ուղարկել</p>
+        </div>
             </div>
+             <img className="h-[400px] w-[900px] rounded-lg" src="https://www.stonemarket.am/_next/image?url=%2Fimages%2Ffeedback.webp&w=1920&q=75" alt="" />
+            </div>
+            <p className="p-4 border-white border w-[1520px]"></p>
+            <img src="https://www.stonemarket.am/icons/logo-light.svg" alt="" />
         </div>
         </>
     );
