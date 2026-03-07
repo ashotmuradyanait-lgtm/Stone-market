cd34c2c367ea551a5bb8b35405412793c52d0b59
Ashot <ashot.muradyan.ait@gmail.com>
2026-03-07 12:21:33 +0400

    add icons for Kap.jsx

--- src/pages/Kap.jsx
+++ src/pages/Kap.jsx
@@ -4,8 +4,15 @@ function Kap(){
     return(
         <>
         <p className="font-semibold  text-center text-3xl">Կոնտակտներ</p>
-        <div></div>
+        <div className="flex gap-30 bg-gray-100 h-[150px] w-[1500px] p-6">
+        <i  class="fa fa-phone" aria-hidden="true">+374 (33) 76 - 73 - 77</i>
+        <i class="fa fa-envelope" aria-hidden="true">sstonemarket@yandex.ru</i>
+        <i class="fa fa-instagram" aria-hidden="true">stonemarket.am</i>
+        <i class="fa fa-facebook-square" aria-hidden="true">Stone Market</i>
+        <i class="fa fa-whatsapp" aria-hidden="true">WhatsApp</i>
+        </div>
         </>
     );
 }
 export default Kap;
+
