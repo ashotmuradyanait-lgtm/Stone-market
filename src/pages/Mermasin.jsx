28ed8db60e1ee9f05eeb5193852bc457d448beef
Ashot <ashot.muradyan.ait@gmail.com>
2026-03-09 23:41:43 +0400

    creat div for img

--- src/pages/Mermasin.jsx
+++ src/pages/Mermasin.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function Mermasin(){
     return(
         <>
-        <div className="bg-gray-900 h-[400px] w-full">
-            <img src="https://www.stonemarket.am/_next/image?url=%2Fimages%2Fpetro-stone-logo.png&w=1920&q=75" alt="" />
+        <div className="bg-gray-900 h-[400px] w-full text-center">
+            <div></div>
+            <img className="h-[100px] w-[100px] p-4" src="https://www.stonemarket.am/_next/image?url=%2Fimages%2Fpetro-stone-logo.png&w=1920&q=75" alt="" />
+            <p className="text-white font-semibold text-3xl">Պետրո Սթոուն</p>
         </div>
         </>
     );
