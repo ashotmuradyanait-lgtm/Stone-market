1dfc044fa647520bf63dae8d7cb3d62e122b92ce
Ashot <ashot.muradyan.ait@gmail.com>
2026-03-09 23:51:40 +0400

    creat div for text Mermasin.jsx

--- src/pages/Mermasin.jsx
+++ src/pages/Mermasin.jsx
@@ -3,11 +3,29 @@ import React from "react";
 function Mermasin(){
     return(
         <>
-        <div className="bg-gray-900 h-[400px] w-full text-center">
-            <div></div>
-            <img className="h-[100px] w-[100px] p-4" src="https://www.stonemarket.am/_next/image?url=%2Fimages%2Fpetro-stone-logo.png&w=1920&q=75" alt="" />
+        <div className="bg-gray-900 h-[300px] w-full text-center p-4">
+            <div className="flex justify-center ">
+                <img className="h-[100px] w-[100px] p-4" src="https://www.stonemarket.am/_next/image?url=%2Fimages%2Fpetro-stone-logo.png&w=1920&q=75" alt="" />
+            </div>
             <p className="text-white font-semibold text-3xl">Պետրո Սթոուն</p>
+            <p className="text-white p-4">
+                «Պետրո Սթոուն» ընկերությունների խումբը զբաղվում է բնական քարերի արդյունահանմամբ, մշակմամբ և վաճառքով՝ ապահովելով <br />
+                 ծառայությունների ամբողջական ցիկլ: Այն իր մեջ ներառում է մի քանի ընկերություն, որոնք առաջարկում են որակյալ լուծումներ <br />
+                 շինարարության, դիզայնի և լանդշաֆտային նախագծերի համար՝ ապահովելով հուսալիություն և երկարատև համագործակցություն <br />
+                 գործընկերների հետ:
+            </p>
         </div>
+        <div className="p-10">
+        <p className="font-semibold text-3xl text-center ">STONE MARKET</p>
+        <br />
+        <h1 className="font-semibold text-xl text-center">Բնական քարի համաշխարհային շուկայի հայկական հարթակը</h1>
+        <br />
+        <p className="text-center font-semibold">Stonemarket-ն առաջին հայկական օնլայն հարթակն է, որը միավորում է բնական քարի արդյունահանմամբ, մշակմամբ և վաճառքով զբաղվող <br />
+         ընկերություններին մեկ տեղում՝ ապահովելով նրանց համար տեսանելիություն, ճանաչելիություն և վստահելի գործընկերային կապեր։ Մենք <br />
+        միավորում ենք արտադրողներին, դիզայներներին, ճարտարապետներին և մատակարարներին՝ ստեղծելով բաց, թափանցիկ և առաջադեմ <br />
+         միջավայր, որտեղ յուրաքանչյուր բիզնես կարող է ներկայացնել իր արտադրանքը, պատմությունը և ոճը։</p>
+       </div>
+       <div></div>
         </>
     );
 }
