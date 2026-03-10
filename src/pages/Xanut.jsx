a990e213c5592be42f94eb1ac778f0a364a21b48
Ashot <ashot.muradyan.ait@gmail.com>
2026-03-10 21:46:20 +0400

    creat div  for crads of Xanut.jsx

--- src/pages/Xanut.jsx
+++ src/pages/Xanut.jsx
@@ -3,9 +3,30 @@ import React from "react";
 function Xanut(){
     return(
         <>
-        <div className="bg-gray-300 w-full h-full">
-            <div></div>
-            <p>Գլխավոր</p>
+        <div className="bg-gray-100 w-full h-full">
+            <div className="flex px-14 gap-2">
+            <p className="text-gray text-[14px]">Գլխավոր</p>
+            <img src="https://www.stonemarket.am/icons/arrow-right-black.svg" alt="" />
+            <p className="text-gray text-[14px]">Խանութ</p>
+            </div>
+            <div className="px-14 p-8 flex gap-2">
+                <p className="font-semibold">Կատեգորիա:</p>
+                <p className="border border-gray rounded-lg h-[35px] w-[200px] text-center hover:border-green-500  ">Բնական քար</p>
+                <p className="border border-gray rounded-lg h-[35px] w-[200px] text-center hover:border-green-500  ">Արհեստական քար</p>
+                <p className="border border-gray rounded-lg h-[35px] w-[130px] text-center hover:border-green-500  ">Հաստոցներ</p>
+                <p className="border border-gray rounded-lg h-[35px] w-[500px] text-center hover:border-green-500  ">Քարամշակման գործիքներ և պարագաներ</p>
+                <p className="border border-gray rounded-lg h-[35px] w-[250px] text-center hover:border-green-500  ">Քիմիական նյութեր</p>
+                <p className="border border-gray rounded-lg h-[35px] w-[400px] text-center hover:border-green-500  ">Արտադրական Ծառայություններ</p>
+            </div>
+            <div className="px-42">
+            <p className="border border-gray rounded-lg h-[35px] w-[130px] text-center hover:border-green-500 ">Mane Tiles</p>
+            </div>
+
+            <div>
+                <div>
+                    
+                </div>
+            </div>
         </div>
         </>
     );
